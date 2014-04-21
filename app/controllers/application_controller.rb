c8c0d2591206d30026f03181808b5a932036540e
Mohnish Thallavajhula <i@mohni.sh>
2014-04-20 23:21:31 -0700

    Cache the user. Lookup only once per request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::Base
 
   private
     def current_user
-      User.find_by(id: session[:user_id])
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
     end
 end
