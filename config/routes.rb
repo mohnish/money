aeddde48b02cfbb6fb748da420c5c44061a2224f
Mohnish Thallavajhula <i@mohni.sh>
2014-04-27 15:08:56 -0700

    Add expenses resource

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Expenses::Application.routes.draw do
   get 'users/verify/:verification_id' => 'users#verify'
 
   resources :users
+  resources :expenses, only: [:create, :update, :destroy]
   resources :sessions, only: [:new, :create, :destroy]
 
   root 'sessions#new'
