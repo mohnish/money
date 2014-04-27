0b4026379946ad2ff35e7b0e03e07c1f62923293
Mohnish Thallavajhula <i@mohni.sh>
2014-04-27 15:09:25 -0700

    Add user factory with expenses

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,4 +4,16 @@ FactoryGirl.define do
     sequence(:email) { |n| "mail_#{n}@example.com" }
     password_digest "awesome_password_digest_123"
   end
+
+  # Tip: This can be done even by nesting this inside the above defined
+  # factory, but, this looks more clear
+  factory :user_with_expenses, parent: :user do
+    ignore do
+      expenses_count 5
+    end
+
+    after(:create) do |user, evaluator|
+      create_list(:expense, evaluator.expenses_count, user: user)
+    end
+  end
 end
