c9686abf3b9f5ee019159dcbba17e8e216764858
Mohnish Thallavajhula <i@mohni.sh>
2014-04-27 15:08:05 -0700

    Add numericality check for price

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -2,6 +2,9 @@ class Expense < ActiveRecord::Base
   belongs_to :user
   has_many :tags
 
-  validates :price, { presence: { message: 'Please enter a price' } }
+  validates :price, {
+    presence: { message: 'Please enter a price' },
+    numericality: { message: 'Please enter a valid price' }
+  }
   validates :store, { presence: { message: 'Please enter a store name' } }
 end
