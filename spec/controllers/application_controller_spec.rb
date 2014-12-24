c42287598a540bc5f60058469a3f0c7b4e272b8e
Mohnish Thallavajhula <i@mohni.sh>
2014-12-24 14:46:34 -0800

    Delete old specs and remove gems

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,25 +1,4 @@
 require 'spec_helper'
 
 describe ApplicationController do
-  describe '#current_user' do
-    let(:user) { FactoryGirl.create :user }
-
-    before do
-      session[:user_id] = user.id
-    end
-
-    it 'returns the current user set in the session' do
-      expect(controller.send(:current_user)).to eql(user)
-    end
-  end
-
-  describe '#logged_in?' do
-    before do
-      controller.stubs(:current_user).returns(build(:user))
-    end
-
-    it 'returns true when logged in' do
-      expect(controller.send(:logged_in?)).to be(true)
-    end
-  end
 end
