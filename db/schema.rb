eb225e1c9fe68faf1bb9079912cf036158507b83
Mohnish Thallavajhula <i@mohni.sh>
2014-12-26 16:40:33 -0800

    add tags

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141227003755) do
+ActiveRecord::Schema.define(version: 20141227003929) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name",       limit: 255, null: false
@@ -28,6 +28,12 @@ ActiveRecord::Schema.define(version: 20141227003755) do
 
   create_table "repeat_intervals", force: :cascade do |t|
     t.string   "interval",   limit: 255, null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name",       limit: 255, null: false
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
   end
