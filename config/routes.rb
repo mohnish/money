85a2ab7b685a86284db34f3f8cfe2a76ec62203e
Mohnish Thallavajhula <i@mohni.sh>
2014-12-29 23:30:26 -0800

    add api/v1/* routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Money::Application.routes.draw do
   use_doorkeeper
 
+  namespace :api do
+    scope module: 'v1' do
+      resources :bills, except: [:edit, :new]
+      resources :tags, only: [:index, :create, :destroy]
+      resources :categories, except: [:edit, :new]
+      resources :users, only: [:show, :create, :update, :destroy]
+      resources :payment_sources, except: [:edit, :new]
+      resources :payments, except: [:edit, :new]
+    end
+  end
 end
