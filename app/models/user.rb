aab0fb8c4a10ccdb6cfa3071db748e6508cad98c
Mohnish Thallavajhula <i@mohni.sh>
2015-01-05 22:42:52 -0800

    check for password only on create

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,10 @@ class User < ActiveRecord::Base
   validates_presence_of :username
   validates_presence_of :first_name
   validates_presence_of :last_name
-  validates_presence_of :password
+  validates :password, {
+    presence: true,
+    on: :create
+  }
   validates :email_address, {
     uniqueness: true,
     format: {
