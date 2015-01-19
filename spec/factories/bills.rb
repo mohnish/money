0a41a8b628149e5acbe41139e5154619db70fe3b
Mohnish Thallavajhula <i@mohni.sh>
2015-01-19 15:26:04 -0800

    make the bills factory a bit nicer

--- spec/factories/bills.rb
+++ spec/factories/bills.rb
@@ -1,10 +1,10 @@
 FactoryGirl.define do
   factory :bill do
     user
-    name "Food"
-    next_due_date Time.now
+    name ['Netflix', 'GitHub', 'Credit Card', 'Library', 'Gym'].sample
+    next_due_date 5.days.since
     category
     repeat_interval
-    amount 9.99
+    amount [9.99, 24.99, 30.00, 7].sample
   end
 end
