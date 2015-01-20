692ca3928eb574a5d333a815a9ea3e2d1840a672
Mohnish Thallavajhula <i@mohni.sh>
2015-01-19 16:03:03 -0800

    add label to repeat interval

--- db/migrate/20141227003236_create_repeat_intervals.rb
+++ db/migrate/20141227003236_create_repeat_intervals.rb
@@ -2,6 +2,7 @@ class CreateRepeatIntervals < ActiveRecord::Migration
   def change
     create_table :repeat_intervals do |t|
       t.string :interval, null: false
+      t.string :label, null: false
 
       t.timestamps null: false
     end
--- spec/factories/repeat_intervals.rb
+++ spec/factories/repeat_intervals.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :repeat_interval do
-    interval '2 months'
+    interval 'two_months'
+    label '2 months'
   end
 end
