2baa482a77fd5de33664e0d65cfba8b36d18cbe5
Mohnish Thallavajhula <i@mohni.sh>
2015-01-25 23:28:31 -0800

    ocd

--- spec/factories/bills.rb
+++ spec/factories/bills.rb
@@ -1,11 +1,11 @@
 FactoryGirl.define do
   factory :bill do
-    user
     name ['Netflix', 'GitHub', 'Credit Card', 'Library', 'Gym'].sample
     next_due_date 5.days.since
-    category
-    repeat_interval
     amount [9.99, 24.99, 30.00, 7].sample
+    repeat_interval
+    category
+    user
 
     factory :bill_with_tags do
       transient do
