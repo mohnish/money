bb058a8ef1475bee529dd6fc7e6539460a64c4c6
Mohnish Thallavajhula <i@mohni.sh>
2015-01-25 19:37:02 -0800

    refactor user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,22 +4,25 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
-  validates_presence_of :email_address
-  validates_presence_of :username
   validates_presence_of :first_name
   validates_presence_of :last_name
+
   validates :password, {
     presence: true,
     on: :create
   }
+
   validates :email_address, {
     uniqueness: true,
+    presence: true,
     format: {
       with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create
     }
   }
+
   validates :username, {
     uniqueness: true,
-    length: { maximum: 12, minimum: 3 }
+    presence: true,
+    length: { maximum: 12, minimum: 2 }
   }
 end
