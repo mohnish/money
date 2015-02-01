9c78287218b3e31b6f045672edbf98a4740f6ab4
Mohnish Thallavajhula <i@mohni.sh>
2015-01-31 17:34:55 -0800

    add specs for welcome controller

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -3,13 +3,17 @@ require 'rails_helper'
 RSpec.describe WelcomeController do
   describe 'GET /signup' do
     it 'renders the signup form' do
+      get :signup
       expect(response).to have_http_status(:ok)
+      expect(response).to render_template(:signup)
     end
   end
 
   describe 'GET /signin' do
     it 'render the signin form' do
+      get :signin
       expect(response).to have_http_status(:ok)
+      expect(response).to render_template(:signin)
     end
   end
 end
