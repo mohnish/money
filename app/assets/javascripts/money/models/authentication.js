dfa2ace4f31f0c1b390ce0348e9abfba19bb03c2
Mohnish Thallavajhula <i@mohni.sh>
2015-02-05 10:38:13 -0800

    add auth model

---
+++ app/assets/javascripts/money/models/authentication.js
@@ -0,0 +1,19 @@
+M.Authentication = Backbone.Model.extend({
+  defaults: {
+    username: '',
+    password: '',
+    access_token: '',
+    grant_type: 'password'
+  },
+
+  url: '/oauth/token',
+
+  validate: function(attrs, options) {
+    var errors = [];
+
+    if(_.isEmpty(attrs.username)) errors.push('username is invalid');
+    if(_.isEmpty(attrs.password)) errors.push('password is invalid');
+
+    if (!_.isEmpty(errors)) return errors;
+  }
+});
