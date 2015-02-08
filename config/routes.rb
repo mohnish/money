b6609cf0630995f2e01e4b0fc6c21d16761645c2
Mohnish Thallavajhula <i@mohni.sh>
2015-02-08 01:42:40 -0800

    update route to call api/users/me and not api/users/id since we use an access token

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Money::Application.routes.draw do
 
   namespace :api, defaults: { format: 'json' } do
     scope module: 'v1' do
-      resources :users, only: [:show, :create, :update, :destroy]
+      get '/users/me', to: 'users#me'
+      resources :users, only: [:create, :update, :destroy]
       resources :payment_sources, only: [:index, :create, :update, :destroy]
       resources :categories, only: [:index]
       resources :bills, except: [:edit, :new] do
