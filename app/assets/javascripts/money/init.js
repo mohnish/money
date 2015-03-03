436a237bf49b6587f963c50461ae5c66d1cd842c
Mohnish Thallavajhula <i@mohni.sh>
2015-03-02 18:47:09 -0800

    condense the condensed bills

--- app/assets/javascripts/money/init.js
+++ app/assets/javascripts/money/init.js
@@ -1,5 +1,6 @@
 
 window.M = window.M || {};
+M.dispatcher = _.clone(Backbone.Events);
 M.Utils = {};
 M.Utils.greetings = [
   'నమస్కారం',
@@ -36,6 +37,38 @@ M.Utils.greetings = [
   'halo'
 ];
 
-M.dispatcher = _.clone(Backbone.Events);
+M.IconMap = {
+  "tv_video": "video-camera",
+  "health_fitness": "heartbeat",
+  "bank": "money",
+  "food_drinks": "cutlery",
+  "home_rent": "home",
+  "car": "car",
+  "insurance": "umbrella",
+  "phone": "phone",
+  "medical": "user-md",
+  "internet": "globe",
+  "electric_utility": "lightbulb-o",
+  "gas": "fire",
+  "water": "tint",
+  "trash": "trash-o",
+  "computer": "laptop",
+  "baby_childcare": "child",
+  "gym_sports": "futbol-o",
+  "subscription": "newspaper-o",
+  "education_loans": "graduation-cap",
+  "furniture": "house", // TODO: change this to something better
+  "appliance": "desktop", // TODO: change this to something better
+  "landscaping_lawn": "tree", // TODO: change this to something better
+  "security_alarm": "user-secret", // TODO: change this to something better
+  "donations": "gift",
+  "legal": "gavel",
+  "business_travel": "suitcase",
+  "mail_post_office_box": "envelope", // TODO: change this to something better
+  "locker_deposit_box": "key", // TODO: change this to something better
+  "shopping": "shopping-cart",
+  "ticket": "ticket",
+  "taxes_general": "file-text"
+};
 
 // TODO: Add analytics here
