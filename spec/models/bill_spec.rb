3e67ba585b1dbd0bc47e9f3a5d0cc6966baf5938
Mohnish Thallavajhula <i@mohni.sh>
2015-03-04 16:09:00 -0800

    move controller methods for bills to the bill modelgst

--- spec/models/bill_spec.rb
+++ spec/models/bill_spec.rb
@@ -1,5 +1,66 @@
 require 'rails_helper'
 
 RSpec.describe Bill do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#update_next_due_date' do
+    context 'when the bill is non-recurring' do
+      let(:bill) { Bill.new(repeat_interval: RepeatInterval.new(interval: 'one_time')) }
+
+      it 'returns nil' do
+        expect(bill.update_next_due_date).to be_nil
+      end
+    end
+
+    context 'when the bill is recurring' do
+      let(:original_date) { Time.zone.now }
+      let(:bill) { create :bill, next_due_date: original_date }
+      let(:expected_due_date) { original_date + 2.months }
+
+      it 'returns the next date based on the repeat interval' do
+        expect(bill.update_next_due_date).to be_truthy
+        expect(bill.reload.next_due_date.strftime("%m/%d/%Y")).to eql(expected_due_date.strftime("%m/%d/%Y"))
+      end
+    end
+  end
+
+  describe '#next_due_date' do
+    context 'when the passed in value is a ActiveSupport::TimeWithZone obj' do
+      let(:date) { Time.zone.now }
+      let(:bill) { Bill.new }
+
+      it 'assigns the value to the next_due_date attribute' do
+        bill.next_due_date = date
+        expect(bill.next_due_date).to eql(date)
+      end
+    end
+
+    context 'when the passed in value is a valid string' do
+      let(:date) { '10/01/1993' }
+      let(:bill) { Bill.new }
+
+      it 'assigns the value to the next_due_date attribute' do
+        bill.next_due_date = date
+        expect(bill.next_due_date.strftime("%m/%d/%Y")).to eql(date)
+      end
+    end
+
+    context 'when the passed in value is an invalid string' do
+      let(:date) { 'asfgasdfa' }
+      let(:bill) { Bill.new }
+
+      it 'will not assign the value to the next_due_date attribute' do
+        bill.next_due_date = date
+        expect(bill.next_due_date).to be_nil
+      end
+    end
+
+    context 'when the passed in value is a random value' do
+      let(:date) { 1213 }
+      let(:bill) { Bill.new }
+
+      it 'will not assign the value to the next_due_date attribute' do
+        bill.next_due_date = date
+        expect(bill.next_due_date).to be_nil
+      end
+    end
+  end
 end
