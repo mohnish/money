fb2d2db14a1548777cab48b727dbb6bb8b6958d4
Mohnish Thallavajhula <i@mohni.sh>
2015-04-12 23:10:37 -0700

    WIP reporting

--- app/controllers/api/v1/reports_controller.rb
+++ app/controllers/api/v1/reports_controller.rb
@@ -1,9 +1,17 @@
 module Api
   module V1
     class ReportsController < BaseController
-      def index
-        @report = current_user.bills
+      def monthly
+        @payments = Payment.joins(:bill).where({
+          bills: { user_id: current_user.id },
+          created_at: date_range
+        })
       end
+
+      private
+        def date_range start_date = Time.now, end_date = Time.now
+          start_date.beginning_of_month..end_date.end_of_month
+        end
     end
   end
 end
